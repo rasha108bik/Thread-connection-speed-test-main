5418d39f5abf6a211a3e2dc94d89cd204c1787b2
rasha108bik <88933433+rasha108bik@users.noreply.github.com>
2021-10-24 00:02:01 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Thread-connection-speed-test-main
+# Thread-connection-speed-test
+
+Функция логирования времени работы метода (файл log_duration.hpp)
+
+Ещё сильнее ускорить ввод и вывод в cin и cout может магическая команда ios_base::sync_with_stdio(false)
---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include "log_duration.h"
+
+#include <iostream>
+
+using namespace std;
+
+class StreamUntier {
+public:
+    StreamUntier(istream& in) {
+        tied_before_ = in.tie(nullptr);
+    }
+
+    ~StreamUntier() {
+        cin.tie(tied_before_);
+    }
+
+private:
+    ostream* tied_before_;
+};
+
+
+int main(int argc, const char** argv) {
+    // не забываем, что один аргумент — это название программы,
+    // поэтому argc должно быть как минимум 2
+    if (argc < 2) {
+        cerr << "Пожалуйста, задайте как минимум 1 аргумент"s << endl;
+        return 1;
+    }
+
+    int arg = std::stoi(argv[1]);
+
+    if (arg == 1) {
+        LOG_DURATION("endl"s);
+        int i;
+        while (cin >> i) {
+            cout << i * i << endl;
+        }
+    }
+
+    if (arg == 2) {
+        LOG_DURATION("\\n"s);
+        int i;
+        while (cin >> i) {
+            cout << i * i << "\n"s;
+        }
+    }
+
+    if (arg == 3) {
+        LOG_DURATION("\\n with tie"s);
+        StreamUntier guard(cin);
+        int i;
+        while (cin >> i) {
+            cout << i * i << "\n"s;
+        }
+    }
+}
